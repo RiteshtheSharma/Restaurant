dfa37d55e58f8bf319695b18eafaa0dc38fff616
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2024-01-20 15:51:46 +0530

    fix : page url routing fixed

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import Inspect from 'vite-plugin-inspect'
 import { resolve } from 'path'
 
 export default {
+    plugins: [Inspect()],
+
     build: {
-        rollupOptions: {
-          input: {
-            main: resolve(__dirname, 'index.html'),
-            menu: resolve(__dirname, 'menu/index.html'),
-            contact : resolve(__dirname, 'contact/index.html')
-          } }},
-  plugins: [
-    Inspect(),
-    
-   
-  ],
-}
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          menu: resolve(__dirname, 'menu/index.html'),
+          contact: resolve(__dirname, 'contact/index.html')
+        }
+      }
+    }
+
+  }
